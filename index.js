aa20adf33c7f60fb710d96f13bf8a7e65e5c0af5
oprr8ebcnl <oprr8ebcnl@gmail.com>
2017-06-26 10:38:53 -0400

    Added to github

---
+++ index.js
@@ -0,0 +1,27 @@
+//First we actually need to import the packages we downloaded
+//Using the require keyword
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http); //IO is going to be how we communicate with clients
+
+//You can think of IO as being the server, passing data between two/more clients
+
+
+//app.get(path, callback)
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+//Use express to serve up static files(css, js, other htmls besides index) so that our page can be pretty
+//We are going to handle the socket events here
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+//Tell the server where it should run on the host
+http.listen([process.env.PORT || 3000, function(){
+	console.log('listening on *:3000');
+});
