f9e5e6c889d6eec55a1c738339b59d2049dd4726
oprr8ebcnl <oprr8ebcnl@gmail.com>
2017-06-27 10:20:18 -0400

    fix

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ io.on('connection', function(socket){
 });
 
 //Tell the server where it should run on the host
-http.listen([process.env.PORT || 3000, function(){
+http.listen(process.env.PORT || 3000, function(){
 	console.log('listening on *:3000');
 });
